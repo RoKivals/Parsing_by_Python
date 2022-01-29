95d948b971259d607e808ebbfd37247d1b0123ba
Slava <slava111103@gmail.com>
2022-01-30 00:50:20 +0300

    #2.py был переделан под наглядный пример об изменении User-Agent путём передачи в заголовок запроса.

--- #2.py
+++ #2.py
@@ -14,9 +14,11 @@ response = requests.get(link, headers=header).text
 soup = BeautifulSoup(response, 'lxml')
 # Ищем блок, который содержит всю необходимую нам информацию (отсюда и название переменной)
 block = soup.find("div", id="tool_padding")
-js_block = block.find('div', id="javascript_check")
-js_status = js_block.find_all('span')[1].text
+ua_block = block.find('div', id="user_agent")
+ua_status = ua_block.text
 
+
+print(ua_status)
 '''
 Принцип работы примерно таков, НО это работает только для статических сайтов, где ничего не подгружается через скрипты.
 В третьем примере будет показана работа lxml по назначению (со статикой).
